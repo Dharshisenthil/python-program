c37bc3c6f8b7ac54a6037817f4e624119344b28e
Dharshisenthil <50619238+Dharshisenthil@users.noreply.github.com>
2019-05-15 11:30:18 +0530

    Update pnz23.py

--- pnz23.py
+++ pnz23.py
@@ -1,7 +1,7 @@
-num=float(input("enter a number:"))
+num=float(input("enter a no:"))
 if num>0:
-print("positive number")
+print("positive no")
 elif num==0:
 print("zero")
 else:
-print("negative number")
+print("negative no")
