9324c374c2081e14df4f0c89d05817235637f17c
Dharshisenthil <50619238+Dharshisenthil@users.noreply.github.com>
2019-05-15 09:06:28 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,5 @@
+ch=input("enter a character:")
+if(ch=='A' or ch=='a' or ch=='E' or ch=='e'or ch=='I' or ch=='i' or ch=='O' or ch=='o'or ch=='U' or ch=='u')
+print(ch,"it is a vowel")
+else:
+print(ch"it is a consonent")
