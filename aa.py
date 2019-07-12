ba830bb68c584067f37a04be37ab2673bf3b0273
Dharshisenthil <50619238+Dharshisenthil@users.noreply.github.com>
2019-07-11 20:08:09 -0700

    Create aa.py

---
+++ aa.py
@@ -0,0 +1,12 @@
+n1=int(input())
+aa=[]
+for i in range(0,n1):
+   aa1=input()
+   aa.append(ss1)
+aa=[]
+for i in zip(*aa):
+ if(i.count(i[0])==len(i)):
+  aa2.append(i[0])
+ else:
+  break
+print(''.join(aa2))
