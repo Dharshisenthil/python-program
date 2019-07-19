72131a94249a95f59d59e67580de334d5b847da3
Dharshisenthil <50619238+Dharshisenthil@users.noreply.github.com>
2019-07-19 13:45:38 +0530

    Update smallestnumber.py

--- smallestnumber.py
+++ smallestnumber.py
@@ -1,13 +1,6 @@
-ha,bb=input().strip().split()
-bb=int(bb)
-a=0
-while h<len(hh)-1 and pp:
- if(a[a]>aa[a+1]):
-  bb-=1
-  aa=aa[:a]+aa[a+1:]
-  if(a!=0):
-   a-=1
- else:
-  a+=1
-lk=aa[:len(aa)-bb]
-print(lk)
+from itertools import combinations
+Si,u=map(int,input().split())
+lis=len(str(Si))
+lst=list(combinations(str(Si),lis-u))
+lst=sorted(lst)
+print(*lst[0],sep='')
