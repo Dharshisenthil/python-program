986624a897ca172c28ebe0044ac9388cade42d85
Dharshisenthil <50619238+Dharshisenthil@users.noreply.github.com>
2019-07-19 17:15:55 +0530

    Create 1pro35.py

---
+++ 1pro35.py
@@ -0,0 +1,20 @@
+ss3=input()
+minimum3=len(ss3)
+d=0
+while(i<minimum3):
+    m3=0
+    k3=0
+    for j in range(len(ss3)):
+        k3+=1
+        if(ss3[d]==ss3[j]):
+            if(k3>m3):
+                m3=k3
+            k3=0
+        if(k3>minimum3):
+            break
+    if(k3>m3):
+        m3=k3
+    if(m3<minimum3):
+        minimum3=m3
+    d+=1
+print(minimum3)
