0f2674d996252a27d0fbff6ce1ef96c1a348ad28
Dharshisenthil <50619238+Dharshisenthil@users.noreply.github.com>
2019-07-19 13:51:40 +0530

    Update baloon.py

--- baloon.py
+++ baloon.py
@@ -1,7 +1,7 @@
 str1,str2=input().split()
 cost_diff=abs(len(str1)-len(str2))
 for i in range(len(str1)):
-    if len(str2)==1 and str2[i] in st1:
+    if len(str2)==1 and str2[i] in str1:
         break
     if str1[i] != str2[i]:
         cost_diff+=1 
