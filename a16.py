6a27663f6724aa417c272456a1126d1f4a12a97e
Dharshisenthil <50619238+Dharshisenthil@users.noreply.github.com>
2019-07-19 14:47:44 +0530

    Update a16.py

--- a16.py
+++ a16.py
@@ -2,10 +2,10 @@ nnn1=int(input())
 li1=list(map(int,input().split()))
 a=[1]*nnn1
 for d in range(nnn1):
-    if(i==0):
-        if(li1[i]>li1[i+1]):
+    if(d==0):
+        if(li1[d]>li1[d+1]):
             a[d]=a[d]+a[d+1]
     elif(d>0):
-        if(li1[i]>li1[i-1]):
+        if(li1[d]>li1[d-1]):
             a[d]=a[d]+a[d-1]
 print(sum(a))
