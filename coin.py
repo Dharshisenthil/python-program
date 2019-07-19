2daef9ddb8e913fde3daeb6f61d51d99237c8b53
Dharshisenthil <50619238+Dharshisenthil@users.noreply.github.com>
2019-07-19 16:35:07 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -1,33 +1,15 @@
-
-# A Naive recursive python program to find minimum of coins 
-  
 import sys 
-  
-# m is size of coins array (number of different coins) 
-def minCoins(coins, m, V): 
-  
-    # base case 
-    if (V == 0): 
+def inCoinspi(coinspi, mo1, Vd): 
+    if (Vd == 0): 
         return 0
-  
-    # Initialize result 
     res = sys.maxsize 
-      
-    # Try every coin that has smaller value than V 
-    for i in range(0, m): 
-        if (coins[i] <= V): 
-            sub_res = minCoins(coins, m, V-coins[i]) 
-  
-            # Check for INT_MAX to avoid overflow and see if 
-            # result can minimized 
+    for i in range(0, mo1): 
+        if (coinspi[i] <= Vd): 
+            sub_res = inCoinspi(coinspi, mo1, Vd-coinspi[i]) 
             if (sub_res != sys.maxsize and sub_res + 1 < res): 
-                res = sub_res + 1
-  
-    return res 
-  
-# Driver program to test above function 
-coins = [9, 6, 5, 1] 
-m = len(coins) 
-V = 11
-print("Minimum coins required is",minCoins(coins, m, V)) 
-  
+                res = sub_res + 1 
+    return res
+n,Vd=list(map(int,input().split()))
+coinspi = list(map(int,input().split())) 
+mo1= len(coinspi) 
+print(inCoinspi(coinspi, mo1, Vd)) 
