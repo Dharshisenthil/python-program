c8f0c19d1bbcaa876eed099be31a3aef8d247444
Dharshisenthil <50619238+Dharshisenthil@users.noreply.github.com>
2019-07-19 17:26:20 +0530

    Update 1pro37.py

--- 1pro37.py
+++ 1pro37.py
@@ -3,9 +3,8 @@ L3 = [int(x) for x in input().split()]
 nnnn3 = len(L3)
 if nnnn3==1 :
     print()
-   
-cnt = 0
-for i in range(1,nnn3-1) :
+   cnt = 0
+for i in range(1,nnnn3-1) :
     if ((L3[i] > L3[i-1]) and (L3[i] > L3[i+1])) or ((L3[i] < L3[i-1]) and (L3[i] < L3[i+1])):
         cnt += 1
 print(cnt)
