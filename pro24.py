5cbfe8363d17499a55580a4fe09fa7a042780466
Dharshisenthil <50619238+Dharshisenthil@users.noreply.github.com>
2019-07-19 16:44:35 +0530

    Create pro24.py

---
+++ pro24.py
@@ -0,0 +1,9 @@
+aaab1=int(input())
+bbb1=pow(2,aab1)
+z11=[]
+for i in range(bbb1):  
+    m1=bin(i).replace("0b","")
+    z11.append(m1.zfill(aaab1))
+    z11.sort(key=(lambda x:x.count('1')))
+for i in z11:
+    print(i)
