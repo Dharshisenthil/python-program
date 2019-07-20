cc39b00e80f64e0a5a4e60cc36f0d1819cc67cc6
Dharshisenthil <50619238+Dharshisenthil@users.noreply.github.com>
2019-07-20 12:15:25 +0530

    Update largest number.py

--- largest number.py
+++ largest number.py
@@ -1,29 +1,8 @@
-# Python3 program to generate largest possible 
-# number with given digits 
-  
-# Function to generate largest possible 
-# number with given digits 
-def findMaxNum(arr,n) : 
-  
-    # sort the given array in 
-    # descending order 
-    arr.sort(reverse = True) 
-  
-    # initialize num with starting 
-    # element of an arr 
-    num = arr[0] 
-  
-    # generate the number 
-    for i in range(1,n) : 
-        num = num * 10 + arr[i] 
-  
-    return num 
-  
-# Driver code 
-if __name__ == "__main__" : 
-      
-    arr = [1,2,3,4,5,0] 
-  
-    n = len(arr) 
-  
-    print(findMaxNum(arr,n)) 
+a=input()
+b=input()
+b=b.split()
+b.sort(reverse = True)
+d=""
+for i in b:
+	d+=i
+print(int(d))
